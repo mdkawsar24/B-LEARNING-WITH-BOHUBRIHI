b49f3cc86df6564eeec27a789cfb53603c339c96
mdkawsar24 <kawser470@yahoo.com>
2020-09-29 10:58:02 +0600

    added one project

--- Full Stack Web Development With Python & JavaScript/Section A(Modern JavaScript Programming)/js/script.js
+++ Full Stack Web Development With Python & JavaScript/Section A(Modern JavaScript Programming)/js/script.js
@@ -224,74 +224,7 @@ console.log("Task Complete!"); */
 
 //P2:Numbers
 
-var a = 0xbb;
-console.log(a);
-a.toString()
-console.log(a);
-var c = a.toString(2)
-console.log(c);
-
-a = a.toString()
-console.log(a);
-var b = a.toString()
-console.log(b);
-
-
-
-
-/* var x;
-x = 3.141;
-x = 34;
-x = 123e5; // 123 * 10 ** 5
-x = 123e-5; // 123 * 10 ** -5
-
-// Numbers takes 64 bits
-
-// + can be used for both addition and concatenation
-"34" + "45" // will be string
-34 + "45" // will be string
-"34" + 45 // will be string
-      "The result is " + 34 + 45
-34 + 45 + "is the result"
-34 + 45 + "13"
-
-//Numeric Strings
-"25" - "5"
-"25" / "5" // result 10
-"25" * "5"
-"25" % "5"
-"25" + "5" //exception
-
-//Infinity
-var num = Infinity;//infinity is also a number
-25 / 0 // will get Infinity
-
-
-//Hexa Decimal Numbers,need to use x for hexadecimal number 
-var x = 0xBB
-
-x.toString() // Returns number as string
-
-x.toString(16);
-
-var x = 3.1416;
-x.toPrecision();
-x.toPrecision(2);
-x.toPrecision(4);
-x.toPrecision(6);
-
-var x = "123"
-parseInt(x);
-x = "123.45"
-parseFloat(x);
-
-isNaN(x);
-
-//Links
-//https://www.w3schools.com/jsref/jsref_obj_number.asp
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number */
 
 //P2:Numbers Ends here
-
 
 //U4:Data Types Ends Here
